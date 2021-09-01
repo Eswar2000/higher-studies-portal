8345daa5d979fe5ebeb7049d316b1087c1b244ac
Sashank <sashankvisweshwaran@gmail.com>
2021-09-01 17:38:25 +0530

    Merged with HD content

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import SignUpScreen from "./screens/SignUpScreen";
 import HomeLayout from "./screens/HomeLayout";
 // import HomeLayout from "./screens/HomeLayout";
 // import ProfileScreen from "./subScreens/ProfileScreen";
+import LandingScreen from "./screens/LandingScreen";
 
 function App() {
   return (
     // <LoginScreen/>
-      <HomeLayout/>
+    //   <HomeLayout/>
       // <CustomSelect/>
-
+      <LandingScreen/>
     // <SignUpScreen/>
   );
 }
