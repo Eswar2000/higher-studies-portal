a9d27fceac151818b92e2fce8bd5bafd08316c27
Harish S.G <harishsg99@gmail.com>
2021-10-21 17:26:22 +0530

    Create profile.java

---
+++ src/main/java/com/ncp/higherstudiesbackend/controllers/profile.java
@@ -0,0 +1,35 @@
+ackage com.ncp.higherstudiesbackend.controllers;
+
+import com.ncp.higherstudiesbackend.enums.AuthStatus;
+import com.ncp.higherstudiesbackend.handlers.AccountHandler;
+import com.ncp.higherstudiesbackend.handlers.UniversityHandler;
+import com.ncp.higherstudiesbackend.utilities.XMLTools;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+@WebServlet(name = "universityServlet",value = "/university")
+
+public class University extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res){
+        try{
+            AuthStatus authStatus= AccountHandler.handleCredentialCheck(req, res);
+
+            if(authStatus == AuthStatus.authenticated){
+                res.setContentType("application/xml");
+                res.setStatus(200);
+                XMLTools.sendXMLResponse(UniversityHandler.getAllUniversities(),res.getWriter(),"universities");
+            }
+
+        }catch(Exception e){
+            res.setStatus(500);
+            e.printStackTrace();
+        }
+    }
+
+}
