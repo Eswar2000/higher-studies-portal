8c9f921e2c21c9db4a19149f9688edc0015f8b27
Eswar2000 <v.eswarraman2000@gmail.com>
2021-11-08 15:08:25 +0530

    signup revert back

--- src/main/java/com/ncp/higherstudiesbackend/controllers/Signup.java
+++ src/main/java/com/ncp/higherstudiesbackend/controllers/Signup.java
@@ -4,17 +4,19 @@ import com.ncp.higherstudiesbackend.handlers.AccountHandler;
 import com.ncp.higherstudiesbackend.utilities.XMLDocument;
 import com.ncp.higherstudiesbackend.utilities.XMLTools;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet(name = "signupServlet",value = "/signup")
 
 public class Signup extends HttpServlet {
 
-    public void doPost(HttpServletRequest req, HttpServletResponse res) {
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
 
         //This is the sample XML
         //<?xml version = "1.0"?>
@@ -30,18 +32,19 @@ public class Signup extends HttpServlet {
 
         try{
             XMLDocument signUpRequestXML=XMLTools.parseXML(req.getInputStream());
-            PrintWriter resWriter = res.getWriter();
             res.setContentType("application/xml");
+            System.out.println("Inside Success Code");
             if(AccountHandler.createUser(signUpRequestXML.getAttributeValue("name"),signUpRequestXML.getAttributeValue("username"),signUpRequestXML.getAttributeValue("email"),signUpRequestXML.getAttributeValue("passwordHash"))){
                 System.out.println("User created");
                 res.setStatus(200);
-                XMLTools.sendXMLResponse(new StringBuilder("<status>Account Created</status>"),resWriter,"response");
+                XMLTools.sendXMLResponse(new StringBuilder("<status>Account Created</status>"),res.getWriter(),"response");
             }
             else {
                 res.setStatus(200);
-                XMLTools.sendXMLResponse(new StringBuilder("<status>Failed To Create New User</status>"),resWriter,"response");
+                XMLTools.sendXMLResponse(new StringBuilder("<status>Failed To Create New User</status>"),res.getWriter(),"response");
             }
         }catch (Exception e) {
+            XMLTools.sendXMLResponse(new StringBuilder("<status>Failed To Create New User</status>"),res.getWriter(),"response");
             res.setStatus(500);
         }
 
