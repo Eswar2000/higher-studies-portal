8eeec17b10bc5b7cd22c93520444bbd4afc55289
Sashank <sashankvisweshwaran@gmail.com>
2021-11-21 09:39:08 +0530

    Removed a workaround and implemented fetching attributes of students properly

--- src/components/CreateStudentTipCard.js
+++ src/components/CreateStudentTipCard.js
@@ -1,22 +1,54 @@
 import {Avatar, IconButton} from "@material-ui/core";
 import CustomInput from "./CustomInput";
 import SendIcon from '@material-ui/icons/Send';
+import {useEffect, useState} from "react";
+import backendService from "../services/backendService";
 
 // eslint-disable-next-line
 export default function CreateStudentTipCard({tipText,handleTipTextChange,onSubmit}){
 
     // eslint-disable-next-line
 
+    const [studentDetails,setStudentDetails]=useState();
+    const [statusCode,setStatusCode]=useState(0);
+
+    const fetchStudentDetails=async ()=>{
+        let response=await backendService("GET","/profile",null,sessionStorage.username,sessionStorage.passwordHash);
+
+        let tempStatusCode=response.statusCode;
+
+        if(response.statusCode !== 200){
+            setStatusCode(response.statusCode);
+            return;
+        }
+        response=response.response;
+        console.log(response);
+
+        setStudentDetails({
+            name: response.studentProfile.name._text,
+            pgUniversityName: response.studentProfile.pgUniversityName._text
+        });
+
+        setStatusCode(tempStatusCode);
+
+
+    }
+
+
+    useEffect(()=>{
+        fetchStudentDetails()
+    },[]);
+
 
 
     return (
         <div className={"universityTipCard"}>
-            <div className={'tipAuthorRow'}>
-                <Avatar sizes={'small'}><b>{sessionStorage.name[0]}</b></Avatar>
+            {statusCode===200 && <div className={'tipAuthorRow'}>
+                <Avatar sizes={'small'}><b>{studentDetails.name[0]}</b></Avatar>
                 <p className={'tipAuthorAddressing'}>Comment as </p>
-                <p className={'tipAuthorText'}>{sessionStorage.name}</p>
-            </div>
-            <p className={'tipAdmittedUniversity'}>{sessionStorage.pgUniversityName}</p>
+                <p className={'tipAuthorText'}>{studentDetails.name}</p>
+            </div>}
+            {statusCode===200 && <p className={'tipAdmittedUniversity'}>{studentDetails.pgUniversityName}</p>}
             <div className={'commentInputDiv'}>
                 {/* <select>
                     <option>Amrita School of Engineering</option>
