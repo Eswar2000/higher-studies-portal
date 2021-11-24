b98f0bb620cb06f7dcfdf346068c9c35d542cd31
Aishwarya <aishwaryababu1212@gmail.com>
2021-11-24 23:26:23 +0530

    state management done

--- src/screens/RepositoryScreen.js
+++ src/screens/RepositoryScreen.js
@@ -3,16 +3,19 @@ import {useRouteMatch} from "react-router";
 import {Link} from 'react-router-dom';
 import {useEffect, useState} from "react";
 import backendService from "../services/backendService";
+import {useHistory} from "react-router";
+import { BsBookmark, BsFillBookmarkFill } from "react-icons/bs";
 
 export default function RepositoryScreen() {
-
+    const history=useHistory();
     const [resources, setResources] = useState([]);
     const [statusCode, setStatusCode] = useState(0);
+    const [bookmarks, setBookmarks] = useState([])
 
     const fetchResources = async() => {
-        let response = await backendService("GET", "/resource", {author: "all"}, sessionStorage.username, sessionStorage.passwordHash);
+
+        let response = await backendService("GET", "/resource?author=all", null, sessionStorage.username, sessionStorage.passwordHash);
         let receivedStatusCode = response.statusCode;
-        console.log(response);
         response = response.response;
 
         let tempResource = [];
@@ -20,8 +23,8 @@ export default function RepositoryScreen() {
         for(let i=0; i<response.resource.length; i++) {
             let resource = {
                 resourceID: response.resource[i].resourceID._text,
-                name: response.resource[i].name._text,
-                author: response.resource[i]._text,
+                name: response.resource[i].resourceName._text,
+                author: response.resource[i].author._text,
                 coverURL: response.resource[i].coverURL._text,
                 storageLocation: response.resource[i].storageLocation._text
             }
@@ -30,6 +33,17 @@ export default function RepositoryScreen() {
 
         setResources(tempResource);
         setStatusCode(receivedStatusCode);
+
+        let response2 = await backendService("GET", "/bookmark", null, sessionStorage.username, sessionStorage.passwordHash);
+        let receivedStatusCode2 = response2.statusCode;
+        response2 = response2.response;
+
+        let tempBookmark = [];
+
+        for(let i=0; i<response2.resourceName.length; i++) {
+            tempBookmark.push(response2.resourceName[i]._text);
+        }
+        setBookmarks(tempBookmark);
     }
 
     const url=useRouteMatch().url;
@@ -50,7 +64,7 @@ export default function RepositoryScreen() {
                     </Link>
                 </div>
                 <div id='books-grid'>
-                    {resources.forEach(resource => {
+                    {resources.map(resource => 
                         <div class='book-tile'>
                             <Card style={{width: '18rem'}}>
                                 <Card.Img variant="top" src={resource.coverURL} />
@@ -58,10 +72,31 @@ export default function RepositoryScreen() {
                                     <Card.Title class='card-title'>{resource.name}</Card.Title>
                                     <Card.Text>{resource.text}</Card.Text>
                                     <Button variant="dark" size="sm">View Resource</Button>
+                                    <div>
+                                       {    
+                                            (bookmarks.includes(resource.resourceID))? (
+
+                                                    <BsFillBookmarkFill style={{float: 'right'}}>
+                                                    </BsFillBookmarkFill>
+                                             ) :(
+                                                    <BsFillBookmarkFill class="bookmark-unmarked" style={{float: 'right'}} onClick={async (e)=> {
+                                                        let reqBody = {
+                                                            resourceID: resource.resourceID,
+                                                            username: sessionStorage.username
+                                                        }
+                                                        let response = await backendService("POST", "/bookmark", reqBody, sessionStorage.username, sessionStorage.passwordHash);
+                                                        if(response.statusCode === 200) {
+                                                            history.replace('/home');
+                                                        }
+                                                    }} ></BsFillBookmarkFill>
+                                             )
+                                        }
+                                    </div>
+                                    
                                 </Card.Body>
                             </Card>
                         </div>
-                    })}
+                    )}
                 </div>
             </div>
         </div>
--- src/subScreens/NewResource.js
+++ src/subScreens/NewResource.js
@@ -1,25 +1,53 @@
+import { useState} from 'react';
 import {Form, Button} from 'react-bootstrap';
+import {useHistory} from "react-router";
+import backendService from "../services/backendService";
+
 
 export default function NewResource() {
+    const [data, setData] = useState({resourceName:"",source:"", coverURL:"", description:""})
+    const history=useHistory();
     return (
         <div>
             <div class="container NewResourceScreen">
             <h1>Add a New Resource</h1>
             <Form>
+                <Form.Group controlId="formName" className="w-50">
+                    <Form.Label>Resource Name</Form.Label>
+                    <Form.Control type="text" name="resourceName" onChange={(e)=>{
+                        setData({...data,[e.target.name]:e.target.value})
+                    }}/>
+                </Form.Group>
                 <Form.Group className="w-50" controlId="formResourceLink">
                     <Form.Label>Link to Resource</Form.Label>
-                    <Form.Control type="text" placeholder="" />
+                    <Form.Control type="text" placeholder="" name="source" onChange={(e)=>{
+                        setData({...data,[e.target.name]:e.target.value})
+                    }}/>
                 </Form.Group>
+                
                 <Form.Group controlId="formFile" id="imageDiv" className="mb-3">
-                    <Form.Label>Add an image</Form.Label>
-                    <Form.Control type="file" />
+                    <Form.Label>Add image URL</Form.Label>
+                    <Form.Control type="text" name="coverURL" onChange={(e)=>{
+                        setData({...data,[e.target.name]:e.target.value})
+                    }}/>
                 </Form.Group>
                 <Form.Group className="w-50" controlId="formDescription">
                     <Form.Label>Description of Resource</Form.Label>
-                    <Form.Control as="textarea" rows={3} />
+                    <Form.Control as="textarea" rows={3} name="description" onChange={(e)=>{
+                        setData({...data,[e.target.name]:e.target.value})
+                    }}/>
                 </Form.Group>
                 <br></br>
-                <Button variant="primary" type="submit">
+                <Button variant="primary" type="submit" onClick={async (e)=>{
+                    e.preventDefault()
+                    let reqBody = {
+                        ...data,
+                        author: sessionStorage.username
+                    };
+                    let response = await backendService("POST", "/resource",reqBody, sessionStorage.username, sessionStorage.passwordHash);
+                    if(response.statusCode === 200){
+                        history.replace('/home');
+                    }}}>
                     Submit
                 </Button>
             </Form>
